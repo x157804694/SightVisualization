ac5f61a033cdcc882deb9d68703d45d09bf4a9c4
goodjack <940457524@qq.com>
2020-05-06 10:47:08 +0800

    实现各月份景区数据可视化

--- sight/src/main/java/com/jxufe/sight/service/ProvinceVisualizationService.java
+++ sight/src/main/java/com/jxufe/sight/service/ProvinceVisualizationService.java
@@ -7,13 +7,16 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface ProvinceVisualizationService {
-    List<ProvinceVisualizationVO> getProvinceSaleCountTop10(String province);
+    List<ProvinceVisualizationVO> getProvinceSaleCountTop10(String province,Integer month);
     List<HashMap<String,Integer>> getPriceOfSight(String province);
-    List<HashMap<String,Integer>> getCitySightNum(String province);
+    List<HashMap<String,Integer>> getCitySightSaleCount(String province,Integer month);
     List<HashMap<String, String>> getStarOfSight(String province);
     List<HashMap<String, ArrayList>> getCityCoord(String province);
     List<HashMap<String, String>> getCitySightNumTop5(String province);
-    Integer getSumSaleCount(String province);
-    List<HashMap<String, Integer>> getSumSaleCountGroupByStar(String province);
-    List<HashMap<String, Integer>> getCitySaleCountTop5(String province);
+    Integer getSumSaleCount(String province,Integer month);
+    List<HashMap<String, Integer>> getSumSaleCountGroupByStar(String province,Integer month);
+    List<HashMap<String, Integer>> getCitySaleCountTop5(String province,Integer month);
+
+    List<HashMap<String, Integer>> getSightSaleOfMonth(String province);
+    List<HashMap<String, String>> getSightCommentNumTop5(String province);
 }
